b32ea3ef2b9dd2d918525fe96cb00b218cbcd8e7
ved11k <mrunal11k@gmail.com>
2026-09-09 16:23:32 +0530

    Add banking documentation site

---
+++ bankingdocumentation/sidebars.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  tutorialSidebar: [
+    'getting-started',
+    'installation',
+    'system-configuration',
+    'user-guide',
+    'administration',
+    'transaction-failures',
+    'troubleshooting',
+  ],
+};
+
+module.exports = sidebars;
